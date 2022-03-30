cbc6e8d095c7b94f9a21f8a95274c815fd7b04b8
Катунова Екатерина <rfneyjdfrfnz@yandex.ru>
2022-03-30 11:29:57 +0300

    change card-model

--- models/card.js
+++ models/card.js
@@ -17,8 +17,9 @@ const cardSchema = new mongoose.Schema({
     required: true,
   },
   likes: [{
-    type: Array,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
+    default: undefined,
   }],
   createdAt: {
     type: Date,
